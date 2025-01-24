366b93e38f145cbdf104a2567f9177fc686ce148
Nguyen Anh Huy <soicaca77@gmail.com>
2025-01-24 10:07:55 +0700

    Upload trang chủ V1.0

--- Project_CNPM_Django/SunDental/home/urls.py
+++ Project_CNPM_Django/SunDental/home/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from.import views
 urlpatterns = [
-    path('', views.home, name = "home")
+    path('', views.home, name = "home"),
+    path('register/', views.register, name = 'register'),
+    path('login/', views.register, name = 'login'),
 ]
