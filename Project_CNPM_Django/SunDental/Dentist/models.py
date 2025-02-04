8fa9621c6f2bc55945d057c45e9c46dd0ea81732
Nguyen Anh Huy <soicaca77@gmail.com>
2025-02-04 09:54:06 +0700

    New ver home and contact

--- Project_CNPM_Django/SunDental/Dentist/models.py
+++ Project_CNPM_Django/SunDental/Dentist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Dentist(models.Model):
+    FullName = models.CharField(max_length=50)
+    Specialization = models.CharField(max_length=255)
+    License_number = models.CharField(max_length=50)
+    Birthday = models.DateField()
+    Gender = models.BooleanField()
